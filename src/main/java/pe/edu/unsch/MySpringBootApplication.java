9ecfdd328df967b198cf7562e9a34e1f1c0502ad
zer-ef <homeboy.puka.crema@gmail.com>
2020-09-07 17:32:11 -0500

    my firt program

---
+++ src/main/java/pe/edu/unsch/MySpringBootApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unsch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootApplication.class, args);
+	}
+
+}
